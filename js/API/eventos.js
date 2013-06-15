e37e1ae0869ddd6be36c76fb56d90bdf35d37f2d
Marco Tulio Cerón <tulioceron@hotmail.com>
2013-06-14 21:31:13 -0500

    Practica 1
    numero 6

--- js/API/eventos.js
+++ js/API/eventos.js
@@ -13,11 +13,14 @@ $(document).ready(function(){
 			{   var db = window.openDatabase("agenda", "1.0", "Agenda BD", 5000000);
 				db.executeSql('CREATE TABLE IF NOT EXISTS eventos (id unique, evento, descripcion, fotos, estado)');
 				db.executeSql('INSERT INTO eventos (evento, descripcion, fotos, estado) VALUES ("'+even+'", "'+desc+'", "'+foto+'", "0")');
+				
 			}
 			else{	navigator.notification.alert('Nombre del evento requerido', null, 'Error de Registro', 'Aceptar');
 				}
 		});
-		
+		$('#leer').tap(function(){
+			leer();
+		});
 		
 		
 	}, false);
