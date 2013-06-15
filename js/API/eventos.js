bb5d9f66aafbb1de13b144aef4fd0b65e5bbcff7
Marco Tulio Cerón <tulioceron@hotmail.com>
2013-06-14 21:06:36 -0500

    Practica 1
    Intento 2

--- js/API/eventos.js
+++ js/API/eventos.js
@@ -8,8 +8,8 @@ $(document).ready(function(){
 		$('#Registrar').tap(function(){
 			var even = $('#evento').val();
 			var desc = $('#descripcion').val();
-			//var foto = $('#tomarFoto').attr('rel');
-			if(nom != '')
+			var foto = $('#tomarFoto').attr('rel');
+			if(even != '')
 			{   var db = window.openDatabase("agenda", "1.0", "Agenda BD", 5000000);
 				db.executeSql('CREATE TABLE IF NOT EXISTS eventos (id unique, evento, descripcion, fotos, estado)');
 				db.executeSql('INSERT INTO eventos (evento, descripcion, fotos, estado) VALUES ("'+even+'", "'+desc+'", "'+foto+'", "0")');
