164f178ceb72b9e9b3b397a937dcb45c3836545e
Marco Tulio Cerón <tulioceron@hotmail.com>
2013-06-21 20:07:57 -0500

    Practica 1.2
    Nuevo intento

--- js/API/eventos.js
+++ js/API/eventos.js
@@ -13,8 +13,8 @@ $(document).ready(function(){
 			if(even != '')
 				{ var db = window.openDatabase("agenda", "1.0", "Agenda BD", 5000000);
 				  db.transaction(function(tx){
-				  db.executeSql('CREATE TABLE IF NOT EXISTS eventos (id unique, evento, descripcion, fotos, estado)');
-				  db.executeSql('INSERT INTO eventos (evento, descripcion, fotos, estado) VALUES ("'+even+'", "'+desc+'", "'+foto+'", "0")');
+				  tx.executeSql('CREATE TABLE IF NOT EXISTS eventos (id unique, evento, descripcion, fotos, estado)');
+				  tx.executeSql('INSERT INTO eventos (evento, descripcion, fotos, estado) VALUES ("'+even+'", "'+desc+'", "'+foto+'", "0")');
 						}, function(){navigator.notification.alert('Error de almacenamiento', null, 'Error de Registro', 'Aceptar');},function(){navigator.notification.alert('Los datos se han guardado', function(){
 				$('#evento').val('');
 				$('#descripcion').val('');
